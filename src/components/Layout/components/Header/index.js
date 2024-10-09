0c012acab867a56112d88c621fee897f4d8eb04b
vanhoang1103 <154491591+vanhoang1103@users.noreply.github.com>
2024-10-09 13:34:39 +0700

    push

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,6 +4,7 @@ import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-s
 import styles from './Header.module.scss';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import images from '~/assets/images';
 import { useEffect, useState } from 'react';
@@ -52,7 +53,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text>Upload</Button>
+                    <Button primary>log in</Button>
+                </div>
             </div>
         </header>
     );
